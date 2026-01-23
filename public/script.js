56d6bdec0a8c1e40d1235fd483b642b7f8683fd8
farafon666 <farafon666@inbox.ru>
2026-01-24 00:10:26 +0400

    feat: add chatting and logging + a little fixes

--- public/script.js
+++ public/script.js
@@ -1,10 +1,17 @@
 const socket = io('/');
+
+/*
+  Signaling block
+  ******************************************************************************************************************************
+*/
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 
 myVideo.muted = true;
 
-const peer = new Peer(undefined, {
+const user = prompt('Enter your name:');
+
+const peer = new Peer({
   path: '/peerjs',
   host: '/',
   port: '3030',
@@ -19,23 +26,23 @@ navigator.mediaDevices.getUserMedia({
 .then(stream => {
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
+
   peer.on('call', call => {
+    console.log('Someone call me');
     call.answer(stream);
     const video = document.createElement('video');
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream);
     });
   });
+
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream);
   });
 });
 
-peer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id);
-});
-
 const connectToNewUser = (userId, stream) => {
+  console.log('I call someone ' + userId);
   const call = peer.call(userId, stream);
   const video = document.createElement('video');
   call.on('stream', userVideoStream => {
@@ -50,3 +57,44 @@ const addVideoStream = (video, stream) => {
     videoGrid.append(video);
   });
 }
+
+peer.on('open', id => {
+  console.log('My id is ' + id);
+  socket.emit('join-room', ROOM_ID, id, user);
+});
+/*
+  ******************************************************************************************************************************
+*/
+/*
+  Chat block
+  ******************************************************************************************************************************
+*/
+const text = document.querySelector('#chatMessage');
+const send = document.getElementById('send');
+const messages = document.querySelector('.messages');
+
+send.addEventListener('click', e =>{
+  if (text.value.length !== 0) {
+    socket.emit('message', text.value);
+    text.value = '';
+  }
+});
+
+text.addEventListener('keydown', e => {
+  if (e.key === 'Enter' && text.value.length !== 0) {
+    socket.emit('message', text.value);
+    text.value = '';
+  }
+});
+
+socket.on('createMessage', (message, userName) => {
+  messages.innerHTML = messages.innerHTML + 
+    `<div class="message">
+        <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName}</span> </b>
+        <p class="message__time">${new Date().toLocaleTimeString()}</p>
+        <span>${message}</span>
+    </div>`;
+});
+/*
+  ******************************************************************************************************************************
+*/
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const {v4: uuidv4 } = require('uuid');
 const io = require('socket.io')(server);
+
 const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
+const peerServer = ExpressPeerServer(server, { debug: true });
+
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use('peerjs', peerServer);
+app.use('/peerjs', peerServer);
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
@@ -21,9 +21,12 @@ app.get('/:room', (req, res) => {
 });
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+  socket.on('join-room', (roomId, userId, userName) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+    socket.on('message', message => {
+      io.to(roomId).emit('createMessage', message, userName);
+    });
   });
 });
 
